93dfce101cd21aea0b4e0a4a735792e1e59e3c97
SharipovNasim <ns.sharipov@gmail.com>
2022-04-15 15:39:26 +0500

    First commit

--- cities/admin.py
+++ cities/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from cities.models import Ciry
+
+admin.site.register(Ciry)
+
 
 # Register your models here.
--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Ciry(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 # Create your models here.
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
