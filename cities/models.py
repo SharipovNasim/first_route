21e6573838759e75d28ebcfab3c4c1dac71b217c
SharipovNasim <ns.sharipov@gmail.com>
2022-04-15 15:54:59 +0500

    First commit

--- cities/models.py
+++ cities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Ciry(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name = 'Город')
 # Create your models here.
     def __str__(self):
         return self.name
