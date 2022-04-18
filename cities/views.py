415956bb4c104810d25a6e8d742aea8f2b6fa04d
SharipovNasim <ns.sharipov@gmail.com>
2022-04-18 14:13:26 +0500

    First commit

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from cities.models import Ciry
+
+__all__ = (
+    'home',
+)
+
+def home(request):
+    qs = Ciry.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
+
+
+
 # Create your views here.
